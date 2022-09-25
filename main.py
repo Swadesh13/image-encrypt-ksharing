19d90db27dcee79ab95512d9f47a7e296ccbf71e
Swadesh13 <swadeshjana@gmail.com>
2022-09-25 12:41:20 +0530

    Minor updates

--- main.py
+++ main.py
@@ -11,36 +11,38 @@ def main():
     parser.add_argument("--img", type=str, default="img.jpg", help="Path to image file")
     parser.add_argument("--task", type=str, choices=["encrypt", "decrypt"], default="encrypt", help="Specify encrypt/decrypt task")
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("--key", type=str, help="Key for encrypting image (min size = 2*n + 1)")
+    group.add_argument("--key", type=str, help="Key for encrypting image (min size = 2*n + 1 digits). Can be alphanumeric.")
     group.add_argument("--key_file", type=str, help="Key file")
     parser.add_argument("--dec_img", type=str, default="img_decrypt.png", help="File to read/write decrypted image.")
     parser.add_argument("--dir", type=str, default="data", help="Directory to store all data.")
     parser.add_argument("-n", type=int, default=5, help="Value of n in (n, k) sharing method.")
-    parser.add_argument("-k", type=int, default=3, help="Value of n in (n, k) sharing method.")
+    parser.add_argument("-k", type=int, default=3, help="Value of k in (n, k) sharing method.")
     parser.add_argument("-w", "--width", type=int, default=0, help="Width of the original image. Required for decrypting image")
 
     args = parser.parse_args()
-    if len(args.key) < 2 * args.n + 1:
-        parser.error("Key length should be at least equal to 2n")
-    elif not args.key.isnumeric():
-        parser.error("Key should be integer")
     if args.task == "decrypt" and args.width == 0:
         parser.error("Require width of original image.")
 
     file_handler = utils.FileReaderWriter(args.dir)
     key = file_handler.read_file(args.key_file, "r") if args.key_file else args.key
+    if not key.isalnum():
+        parser.error("Key must be alphanumeric.")
+    elif not key.isnumeric():
+        key = str(int(key, 36))
+    if len(key) < 2 * args.n + 1:
+        parser.error("Int key length should be at least equal to 2n. Given length:", len(key))
 
     if args.task == "encrypt":
         img_data = imread(os.path.join(args.dir, args.img))
         shape = img_data.shape
 
-        key1 = int(args.key[2 * args.n :])
+        key1 = int(key[2 * args.n :])
         generator = np.random.RandomState(np.random.PCG64(np.random.SeedSequence(key1)))
         mask = generator.randint(0, 255, shape)
         img = ((img_data + mask) % 256).astype(np.uint8)
         imsave(os.path.join(args.dir, "img_encrypt.png"), img)
 
-        key2 = int(args.key[: 2 * args.n])
+        key2 = int(key[: 2 * args.n])
         shares = utils.nk_shares(img, args.n, args.k)
         n_keys = utils.randomize_key2(key2, args.n, generator)
         encrypted_shares = []
@@ -53,8 +55,8 @@ def main():
     elif args.task == "decrypt":
         share_files = [f"share-{i}.png" for i in range(args.n)]
         shares = [imread(os.path.join(args.dir, f)) for f in share_files]
-        key1 = int(args.key[2 * args.n :])
-        key2 = int(args.key[: 2 * args.n])
+        key1 = int(key[2 * args.n :])
+        key2 = int(key[: 2 * args.n])
         generator = np.random.RandomState(np.random.PCG64(np.random.SeedSequence(key1)))
         mask = generator.randint(0, 255, (shares[0].shape[0], args.width, *shares[0].shape[2:]))
         n_keys = utils.randomize_key2(key2, args.n, generator)
